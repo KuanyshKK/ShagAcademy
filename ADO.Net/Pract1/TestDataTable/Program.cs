afc2d472b4f428f982d16762f4c71ce3945532c9
Kuanysh <mrkunyabai@gmail.com>
2019-04-19 10:56:45 +0600

    ADO.Net added

---
+++ ADO.Net/Pract1/TestDataTable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestDataTable
+{
+    class Program
+    {
+        SqlConnection conn = null;
+
+        public Program()
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+        }
+
+         
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+         static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
