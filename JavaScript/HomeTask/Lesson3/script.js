667f9f4f0c0b94d11fd75c1bcafc5ad7899f5d13
Kuanysh <mrkunyabai@gmail.com>
2019-06-27 18:54:27 +0600

    added

--- JavaScript/HomeTask/Lesson3/script.js
+++ JavaScript/HomeTask/Lesson3/script.js
@@ -1,14 +1,13 @@
 class TV {
     constructor(brand,chanel,volume){
-        console.log("this",this);
-
+        //console.log("this",this);
         this.brand = brand;
-        this.chanel = chanel;
-        this.volume = volume;
+        this.chanel = 1;
+        this.volume = 50;
     }
 
     get describeTV(){
-        return `${this.brand} ${this.chanel} ${this.volume}`;
+        return `${this.brand} at chanel ${this.chanel}, volume ${this.volume}`;
     }
 
     set brandSetter(brand){
@@ -20,7 +19,50 @@ class TV {
     }
 
     set volumeSetter(volume){
+        if(volume<0){
+            console.log('Volume must be higher 0');
+        }
+        if(volume>100){
+            console.log('Volume must be lower 100');
+        }
         this.volume = volume;
     }
     
+    changeVolume(vol){
+        if(vol<0){
+            console.log('Volume must be higher 0');
+            return this.volume = this.volume;
+        }
+        if(vol>100){
+            console.log('Volume must be lower 100');
+            return this.volume = this.volume;
+        }
+        return this.volume = vol;
+    };
+
+    changeChanel(ch){
+        if(ch>50){
+            console.log('Chanel must be lower 50');
+            return this.chanel = this.chanel;
+        }        
+        if(ch<0){
+            console.log('Chanel must be higher 0');
+            return this.chanel = this.chanel;
+        }
+        return this.chanel = ch;
+    }
+
+    resetTV(){
+        this.volume = 50;
+        this.chanel = 1;
+    }
+    
 }
+
+var samsung = new TV('samsung');
+samsung.changeVolume(18);
+console.log(samsung.describeTV);
+samsung.changeChanel(45);
+console.log(samsung.describeTV);
+samsung.resetTV();
+console.log(samsung.describeTV);
